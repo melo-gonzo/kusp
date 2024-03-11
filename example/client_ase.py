54fccf1f8522cb483969eefcb9fcdf50aa14fe55
Gonzales, Carmelo <carmelo.gonzales@intel.com>
2024-03-11 10:56:56 -0700

    adding notes to client.ase.py

--- example/client_ase.py
+++ example/client_ase.py
@@ -17,6 +17,7 @@ from matsciml.datasets.transforms import (
 # Initialize KIM Model
 model = KIM("KUSP__MO_000000000000_000")
 
+# Load up a dataset from matsciml
 dset = MaterialsProjectDataset.from_devset(
     transforms=[
         PeriodicPropertiesTransform(cutoff_radius=6.5),
@@ -24,18 +25,22 @@ dset = MaterialsProjectDataset.from_devset(
         MGLDataTransform(),
     ],
 )
-for idx in range(len(dset)):
-    sample = dset.__getitem__(idx)
-    config = Atoms(
-        sample["graph"].ndata["node_type"], positions=sample["graph"].ndata["pos"]
-    )
-    # Setting this manually - matsciml PeriodicPropertiesTransform enforces pbc. Not sure if needed
-    config.pbc = (True, True, True)
-    # Set it as calculator
-    config.set_calculator(model)
-    # Compute energy/forces
-    energy = config.get_potential_energy()
-    forces = config.get_forces()
 
-    print(f"Forces: {forces}")
-    print(f"Energy: {energy}")
+# Grab a sample
+sample = dset.__getitem__(0)
+# Make atoms object
+config = Atoms(
+    sample["graph"].ndata["node_type"], positions=sample["graph"].ndata["pos"]
+)
+# Setting this manually - matsciml PeriodicPropertiesTransform enforces pbc. Not sure if needed
+config.pbc = (True, True, True)
+# Set it as calculator
+config.set_calculator(model)
+# Compute energy/forces
+
+# Currently fails due to the KIM model KUSP__MO_000000000000_000 only supporting Si and O.
+energy = config.get_potential_energy()
+forces = config.get_forces()
+
+print(f"Forces: {forces}")
+print(f"Energy: {energy}")
