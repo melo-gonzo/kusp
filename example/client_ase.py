5043abb8b83f8df3746f1e81f7eab8f7b9e121e0
Gonzales, Carmelo <carmelo.gonzales@intel.com>
2024-03-11 10:41:03 -0700

    update to example scripts

--- example/client_ase.py
+++ example/client_ase.py
@@ -1,18 +1,41 @@
+import sys
+
+sys.path.append("/store/code/open-catalyst/public-repo/matsciml")
 import numpy as np
-from ase import Atoms, io
 from ase.calculators.kim import KIM
+
+from ase import Atoms, io
+
+
+from matsciml.datasets import S2EFDataset, MaterialsProjectDataset
+from matsciml.datasets.transforms import (
+    MGLDataTransform,
+    PeriodicPropertiesTransform,
+    PointCloudToGraphTransform,
+)
 
 # Initialize KIM Model
 model = KIM("KUSP__MO_000000000000_000")
 
-config = io.read("./Si.xyz")
+dset = MaterialsProjectDataset.from_devset(
+    transforms=[
+        PeriodicPropertiesTransform(cutoff_radius=6.5),
+        PointCloudToGraphTransform(backend="dgl"),
+        MGLDataTransform(),
+    ],
+)
+for idx in range(len(dset)):
+    sample = dset.__getitem__(idx)
+    config = Atoms(
+        sample["graph"].ndata["node_type"], positions=sample["graph"].ndata["pos"]
+    )
+    # Setting this manually - matsciml PeriodicPropertiesTransform enforces pbc. Not sure if needed
+    config.pbc = (True, True, True)
+    # Set it as calculator
+    config.set_calculator(model)
+    # Compute energy/forces
+    energy = config.get_potential_energy()
+    forces = config.get_forces()
 
-# Set it as calculator
-config.set_calculator(model)
-
-# Compute energy/forces
-energy = config.get_potential_energy()
-forces = config.get_forces()
-
-print(f"Forces: {forces}")
-print(f"Energy: {energy}")
+    print(f"Forces: {forces}")
+    print(f"Energy: {energy}")
