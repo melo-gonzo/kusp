ca50e6d5ae873f0925b5d7fba1e5778d55199e21
Gonzales, Carmelo <carmelo.gonzales@intel.com>
2024-03-12 16:06:13 -0700

    updating example with atom list and matsciml sample snagger

--- example/client_ase.py
+++ example/client_ase.py
@@ -1,46 +1,22 @@
 import sys
 
 sys.path.append("/store/code/open-catalyst/public-repo/matsciml")
-import numpy as np
+
+from ase import io
 from ase.calculators.kim import KIM
-
-from ase import Atoms, io
-
-
-from matsciml.datasets import S2EFDataset, MaterialsProjectDataset
-from matsciml.datasets.transforms import (
-    MGLDataTransform,
-    PeriodicPropertiesTransform,
-    PointCloudToGraphTransform,
-)
 
 # Initialize KIM Model
 model = KIM("KUSP__MO_000000000000_000")
 
-# Load up a dataset from matsciml
-dset = MaterialsProjectDataset.from_devset(
-    transforms=[
-        PeriodicPropertiesTransform(cutoff_radius=6.5),
-        PointCloudToGraphTransform(backend="dgl"),
-        MGLDataTransform(),
-    ],
-)
-
-# Grab a sample
-sample = dset.__getitem__(0)
-# Make atoms object
-config = Atoms(
-    sample["graph"].ndata["node_type"], positions=sample["graph"].ndata["pos"]
-)
-# Setting this manually - matsciml PeriodicPropertiesTransform enforces pbc. Not sure if needed
-config.pbc = (True, True, True)
+# Using this as a 'dummy config' for now.
+config = io.read("./Si.xyz")
 # Set it as calculator
-config.set_calculator(model)
+i = 0
+
 # Compute energy/forces
-
-# Currently fails due to the KIM model KUSP__MO_000000000000_000 only supporting Si and O.
+config.set_calculator(model)
 energy = config.get_potential_energy()
+# forces returned are not the right shape for some reason. correct in serve_matsciml_models.py
 forces = config.get_forces()
-
-print(f"Forces: {forces}")
 print(f"Energy: {energy}")
+print(f"Forces: {forces}")
--- example/serve_matsciml_models.py
+++ example/serve_matsciml_models.py
@@ -1,24 +1,49 @@
 import sys
 
 sys.path.append("/store/code/open-catalyst/public-repo/matsciml")
-import dgl
 import numpy as np
 import torch
-from ase import Atoms
 from kusp import KUSPServer
 from matsciml.datasets import S2EFDataset
-from matsciml.datasets.transforms import (MGLDataTransform,
-                                          PeriodicPropertiesTransform,
-                                          PointCloudToGraphTransform)
+from matsciml.datasets.transforms import (
+    MGLDataTransform,
+    PeriodicPropertiesTransform,
+    PointCloudToGraphTransform,
+)
 from matsciml.datasets.utils import element_types
-from matsciml.lightning.data_utils import MatSciMLDataModule
 from matsciml.models import M3GNet
 from matsciml.models.base import ScalarRegressionTask
-from pymatgen.io.ase import AseAtomsAdaptor
 
-#########################################################################
-#### Server
-#########################################################################
+
+### Set up sampling from a matsciml dataset
+### How can we use something like this to evaluate a lot of configurations?
+
+
+class MatSciMLSampleGrabber:
+    def __init__(self):
+        self.sample_idx = 0
+        self.dset = S2EFDataset.from_devset(
+            transforms=[
+                PeriodicPropertiesTransform(cutoff_radius=6.5, adaptive_cutoff=True),
+                PointCloudToGraphTransform(backend="dgl"),
+                MGLDataTransform(),
+            ],
+        )
+
+    def grab_sample(self):
+        # Load up a sample from matsciml dataset
+        sample = self.dset.__getitem__(self.sample_idx)
+        self.sample_idx += 1
+        print(self.sample_idx)
+        return sample
+
+
+sampler = MatSciMLSampleGrabber()
+
+
+# #########################################################################
+# #### Server
+# #########################################################################
 
 
 class M3GNetServer(KUSPServer):
@@ -26,22 +51,21 @@ class M3GNetServer(KUSPServer):
         super().__init__(model, configuration)
         self.cutoff = self.global_information.get("cutoff", 6.0)
         self.n_atoms = -1
-        self.config = None
 
     def prepare_model_inputs(self, atomic_numbers, positions, contributing_atoms):
-        import pdb; pdb.set_trace()
-        atomic_numbers = self.graph_in['graph'].ndata['atomic_numbers']
+        self.graph_in = sampler.grab_sample()
+        atomic_numbers = self.graph_in["graph"].ndata["atomic_numbers"]
         self.n_atoms = atomic_numbers.shape[0]
-        self.graph_in['graph'].ndata["node_type"] = torch.tensor(
+        self.graph_in["graph"].ndata["node_type"] = torch.tensor(
             list(map(lambda x: x - 1, atomic_numbers))
         ).to(int)
-        self.graph_in['graph'].ndata['pos'].requires_grad_(True)
+        self.graph_in["graph"].ndata["pos"].requires_grad_(True)
         return {"batch": self.graph_in}
 
     def prepare_model_outputs(self, energies):
         energy = energies["energy_total"]
         energy.backward()
-        forces_contributing = -1 * self.graph_in['graph'].ndata["pos"].grad
+        forces_contributing = -1 * self.graph_in["graph"].ndata["pos"].grad
         forces = np.zeros((self.n_atoms, 3))
         forces[: forces_contributing.shape[0], :] = (
             forces_contributing.double().detach().numpy()
