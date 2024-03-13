a7ae9d8b3f0d4e18aacfacfe13048a0b2516f1c3
Gonzales, Carmelo <carmelo.gonzales@intel.com>
2024-03-13 15:48:05 -0700

    revereting to working end-to-end working example

--- example/client_ase.py
+++ example/client_ase.py
@@ -2,21 +2,21 @@ import sys
 
 sys.path.append("/store/code/open-catalyst/public-repo/matsciml")
 
-from ase import io
+import numpy as np
+from ase import Atoms, io
 from ase.calculators.kim import KIM
 
 # Initialize KIM Model
 model = KIM("KUSP__MO_000000000000_000")
 
-# Using this as a 'dummy config' for now.
 config = io.read("./Si.xyz")
+
 # Set it as calculator
-i = 0
+config.set_calculator(model)
 
 # Compute energy/forces
-config.set_calculator(model)
 energy = config.get_potential_energy()
-# forces returned are not the right shape for some reason. correct in serve_matsciml_models.py
 forces = config.get_forces()
-print(f"Energy: {energy}")
+
 print(f"Forces: {forces}")
+print(f"Energy: {energy}")
--- example/serve_matsciml_models.py
+++ example/serve_matsciml_models.py
@@ -1,64 +1,96 @@
 import sys
 
 sys.path.append("/store/code/open-catalyst/public-repo/matsciml")
+
+import dgl
 import numpy as np
 import torch
-from kusp import KUSPServer
-from matsciml.datasets import S2EFDataset
-from matsciml.datasets.transforms import (
-    MGLDataTransform,
-    PeriodicPropertiesTransform,
-    PointCloudToGraphTransform,
-)
+from ase import Atoms
 from matsciml.datasets.utils import element_types
+from matsciml.lightning.data_utils import MatSciMLDataModule
 from matsciml.models import M3GNet
 from matsciml.models.base import ScalarRegressionTask
+from pymatgen.io.ase import AseAtomsAdaptor
+
+from kusp import KUSPServer
 
 
-### Set up sampling from a matsciml dataset
-### How can we use something like this to evaluate a lot of configurations?
+def raw_data_to_atoms(species, pos, contributing, cell, elem_map):
+    contributing = contributing.astype(int)
+    pos_contributing = pos[contributing == 1]
+    species = np.array(list(map(lambda x: elem_map[x], species)))
+    species = species[contributing == 1]
+    atoms = Atoms(species, positions=pos_contributing, cell=cell, pbc=[1, 1, 1])
+    return atoms
 
 
-class MatSciMLSampleGrabber:
-    def __init__(self):
-        self.sample_idx = 0
-        self.dset = S2EFDataset.from_devset(
-            transforms=[
-                PeriodicPropertiesTransform(cutoff_radius=6.5, adaptive_cutoff=True),
-                PointCloudToGraphTransform(backend="dgl"),
-                MGLDataTransform(),
-            ],
-        )
+def dgl_from_coords(conf: Atoms, cutoff=6.0):
+    pymat_conf = AseAtomsAdaptor.get_structure(conf)
+    pos = torch.as_tensor(conf.get_positions()).to(torch.float)
+    pos.requires_grad_(True)
+    cell = conf.get_cell()[:]
+    cell = torch.as_tensor(cell)
+    from_edge, to_edge, offset, bond_lengths = pymat_conf.get_neighbor_list(
+        r=cutoff, exclude_self=True
+    )
+    offset = torch.as_tensor(offset)
+    bond_lengths = torch.as_tensor(bond_lengths)
+    offshift = offset @ cell
 
-    def grab_sample(self):
-        # Load up a sample from matsciml dataset
-        sample = self.dset.__getitem__(self.sample_idx)
-        self.sample_idx += 1
-        print(self.sample_idx)
-        return sample
+    bond_vecs = pos[from_edge] - pos[to_edge]
+    shifted_bond_vecs = bond_vecs - offshift
+
+    graph = dgl.graph(np.array([from_edge, to_edge]).T.tolist())
+    cells = torch.ones((len(from_edge), 3, 3))
+    cells *= cell
+
+    g = {}
+
+    graph.ndata["node_type"] = torch.tensor(
+        list(map(lambda x: x - 1, pymat_conf.atomic_numbers))
+    ).to(int)
+    graph.ndata["atomic_numbers"] = torch.LongTensor(pymat_conf.atomic_numbers).to(int)
+    graph.ndata["pos"] = pos
+    graph.edata["pbc_offset"] = offset.to(torch.float)
+    graph.edata["pbc_offshift"] = offshift.to(torch.float)
+    graph.edata["lattice"] = cells
+    graph.edata["bond_vec"] = shifted_bond_vecs
+    graph.edata["bond_dist"] = bond_lengths
+
+    g["graph"] = graph
+    g["atomic_numbers"] = torch.LongTensor(pymat_conf.atomic_numbers).to(int)
+    return g
 
 
-sampler = MatSciMLSampleGrabber()
-
-
-# #########################################################################
-# #### Server
-# #########################################################################
+#########################################################################
+#### Server
+#########################################################################
 
 
 class M3GNetServer(KUSPServer):
     def __init__(self, model, configuration):
         super().__init__(model, configuration)
         self.cutoff = self.global_information.get("cutoff", 6.0)
+        self.elem_map = self.global_information.get("elements")
+        self.graph_in = None
+        self.cell = self.global_information.get(
+            "cell",
+            np.array(
+                [[10.826 * 2, 0.0, 0.0], [0.0, 10.826 * 2, 0.0], [0.0, 0.0, 10.826 * 2]]
+            ),
+        )
+        if not isinstance(self.cell, np.ndarray):
+            self.cell = np.array(self.cell)
         self.n_atoms = -1
+        self.config = None
 
     def prepare_model_inputs(self, atomic_numbers, positions, contributing_atoms):
-        self.graph_in = sampler.grab_sample()
-        atomic_numbers = self.graph_in["graph"].ndata["atomic_numbers"]
         self.n_atoms = atomic_numbers.shape[0]
-        self.graph_in["graph"].ndata["node_type"] = torch.tensor(
-            list(map(lambda x: x - 1, atomic_numbers))
-        ).to(int)
+        config = raw_data_to_atoms(
+            atomic_numbers, positions, contributing_atoms, self.cell, self.elem_map
+        )
+        self.graph_in = dgl_from_coords(config, cutoff=self.cutoff)
+        self.config = config
         self.graph_in["graph"].ndata["pos"].requires_grad_(True)
         return {"batch": self.graph_in}
 
